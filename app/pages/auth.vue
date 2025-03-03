6b5979c4465959dd38d7c774de3cb94deed70946
KADIONGO ILUNGA Epaphrodite <kadiongo.ilunga@gecamines.cd>
2025-03-03 21:35:40 +0200

    sozal work with supabase

---
+++ app/pages/auth.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        This is authentication page
+    </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+    title: 'Sozal',
+    link: [
+        { rel: 'icon', href: '/favicon.ico' }
+    ],
+    htmlAttrs: {
+        lang: 'fr'
+    },
+})
+</script>
+
+<style scoped></style>
